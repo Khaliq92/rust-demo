e626983e396de83df56884a3bb97ac41fbe85f8a
Stephen K <steve.at.nyu1@gmail.com>
2017-12-06 12:47:02 -0500

    update setup with uspop dataset

--- rust-df-test/setup.sh
+++ rust-df-test/setup.sh
@@ -9,4 +9,6 @@ set -x
     wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/ncbiRefSeq.txt.gz
 } && {
     gunzip ncbiRefSeq.txt.gz
+} && {
+    curl -LO 'https://raw.githubusercontent.com/BurntSushi/rust-csv/master/examples/data/uspop.csv'
 }
--- rust-df-test/src/main.rs
+++ rust-df-test/src/main.rs
@@ -4,10 +4,11 @@ use utah::prelude::*;
 
 fn main() {
     println!("Hello, world!");
-    
+
     // let file_name = "ncbiRefSeq.txt";
     // let df: Result<DataFrame<f64>> = DataFrame::read_tsv(file_name);
-    let df: Result<DataFrame<f64>> = DataFrame::read_csv("summary-combined.wes.csv");
-    
-    
+    // let df: Result<DataFrame<f64>> = DataFrame::read_csv("summary-combined.wes.csv");
+    let df: Result<DataFrame<f64>> = DataFrame::read_csv("uspop.csv");
+
+
 }
